ff948f0fdd20768a7b4a4c2d15847aedab663798
hakansaglam <hakan.saglam@qconnex.com>
2021-08-03 14:37:07 +0200

    socket.io message handling added

---
+++ src/app.gateway.ts
@@ -0,0 +1,41 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Socket, Server } from 'socket.io';
+const messages = [];
+
+@WebSocketGateway()
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('AppGateway');
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, payload: string): void {
+    if (payload === 'delete') {
+      messages.pop();
+    } else {
+      messages.push(payload);
+    }
+    this.server.emit('msgToClient', [...messages]);
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+}
